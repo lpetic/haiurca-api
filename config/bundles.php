3e194753aa1960a2ee95849d60fb84be807e4f9a
Lucian Petic <lucian.petic14@gmail.com>
2020-12-11 03:27:22 +0100

    update...

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true], //errors here
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true], //errors here
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
